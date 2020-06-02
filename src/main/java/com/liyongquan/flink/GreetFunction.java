7c0e973ed5bf871654d403b7d1238bbe9eec6867
yongquanli <yongquanli@kugou.net>
2020-06-02 23:20:26 +0800

    1

--- src/main/java/com/liyongquan/flink/GreetFunction.java
+++ src/main/java/com/liyongquan/flink/GreetFunction.java
@@ -1,11 +1,13 @@
 package com.liyongquan.flink;
 
+import com.liyongquan.flink.Greeting.GreetRequest;
+import com.liyongquan.flink.Greeting.GreetResponse;
 import org.apache.flink.statefun.sdk.Context;
 import org.apache.flink.statefun.sdk.StatefulFunction;
 
 public final class GreetFunction implements StatefulFunction {
     @Override
-    public void invoke(Context context, Object o) {
+    public void invoke(Context context, Object input) {
         GreetRequest greetMessage = (GreetRequest) input;
 
         GreetResponse response = GreetResponse.newBuilder()
@@ -14,5 +16,20 @@ public final class GreetFunction implements StatefulFunction {
                 .build();
 
         context.send(GreetingConstants.GREETING_EGRESS_ID, response);
+    }
+
+    private static String greetText(String name, int seen) {
+        switch (seen) {
+            case 0:
+                return String.format("Hello %s !", name);
+            case 1:
+                return String.format("Hello again %s !", name);
+            case 2:
+                return String.format("Third times the charm! %s!", name);
+            case 3:
+                return String.format("Happy to see you once again %s !", name);
+            default:
+                return String.format("Hello at the %d-th time %s", seen + 1, name);
+        }
     }
 }
